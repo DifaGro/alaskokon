c74a46361abc1e83f277dd46ff246bc7e834375f
Tofan Adi Nugroho <94297488+DifaGro@users.noreply.github.com>
2025-07-15 22:50:08 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
-    $servername = "localhost"; // Nama server database
-    $username = "root"; // Username database
-    $password = ""; // Password database
-    $dbname = "desa"; // Nama database
+    $servername = "db_alaskokon"; // Gunakan nama servicenya di Coolify
+    $username = "webdesa_user";   // Sesuai field "Normal User"
+    $password = "user12345";      // Sesuai field "Normal User Password"
+    $dbname = "desa";             // Sesuai field "Initial Database"
 
     // Membuat koneksi ke database MySQL
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     // Cek koneksi
     if ($conn->connect_error) {
-        die("Koneksi gagal: " . $conn->connect_error); // Tampilkan error jika koneksi gagal
+        die("Koneksi gagal: " . $conn->connect_error);
     }
-?>
+?
